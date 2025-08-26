66dd37eb0346195503bcee2d237f727b4d03268e
nsolasegmail <105149499+nsolasegmail@users.noreply.github.com>
2025-08-26 01:57:42 -0700

    Delete engagement_classification.sql

--- engagement_classification.sql
+++
@@ -1,9 +0,0 @@
-{% macro classify_engagement_level(total_queries, active_days) %}
-  case
-    when {{ total_queries }} >= 20 and {{ active_days }} >= 15 then 'High Frequency User'
-    when {{ total_queries }} >= 10 and {{ active_days }} >= 8 then 'Mid Frequency User'
-    when {{ total_queries }} >= 5 and {{ active_days }} >= 3 then 'Low Frequency User'
-    when {{ total_queries }} >= 1 then 'Occasional User'
-    else 'Inactive User'
-  end
-{% endmacro %}
