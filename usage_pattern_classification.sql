6e9533c6cd8001d8768dde9ec4cdbb1cd9e1909d
nsolasegmail <105149499+nsolasegmail@users.noreply.github.com>
2025-08-26 01:58:25 -0700

    Delete usage_pattern_classification.sql

--- usage_pattern_classification.sql
+++
@@ -1,9 +0,0 @@
-{% macro classify_usage_pattern(assistant_queries, vault_queries, workflow_queries) %}
-  case 
-    when {{ assistant_queries }} > {{ vault_queries }} and {{ assistant_queries }} > {{ workflow_queries }} then 'Assistant-Focused'
-    when {{ vault_queries }} > {{ assistant_queries }} and {{ vault_queries }} > {{ workflow_queries }} then 'Vault-Focused'
-    when {{ workflow_queries }} > {{ assistant_queries }} and {{ workflow_queries }} > {{ vault_queries }} then 'Workflow-Focused'
-    when {{ assistant_queries }} = {{ vault_queries }} and {{ vault_queries }} = {{ workflow_queries }} then 'Balanced Usage'
-    else 'Mixed Usage'
-  end
-{% endmacro %}
