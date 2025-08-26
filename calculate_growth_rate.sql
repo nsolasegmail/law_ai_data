94b99feb35e8d4e5a777bcd8db13be3428c7f32e
nsolasegmail <105149499+nsolasegmail@users.noreply.github.com>
2025-08-26 01:57:19 -0700

    Delete calculate_growth_rate.sql

--- calculate_growth_rate.sql
+++
@@ -1,7 +0,0 @@
-{% macro calculate_growth_rate(current_value, previous_value) %}
-  case 
-    when {{ previous_value }} > 0 
-    then round((({{ current_value }} - {{ previous_value }})::float / {{ previous_value }} * 100), 2)
-    else null 
-  end
-{% endmacro %}
