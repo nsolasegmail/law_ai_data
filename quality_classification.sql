470dd69a83dc6554d1f119b53060ff0830ee5ac1
nsolasegmail <105149499+nsolasegmail@users.noreply.github.com>
2025-08-26 01:57:53 -0700

    Delete quality_classification.sql

--- quality_classification.sql
+++
@@ -1,8 +0,0 @@
-{% macro classify_quality_level(avg_feedback_score) %}
-  case 
-    when {{ avg_feedback_score }} >= 4.5 then 'High Quality'
-    when {{ avg_feedback_score }} >= 3.5 then 'Good Quality'
-    when {{ avg_feedback_score }} >= 2.5 then 'Average Quality'
-    else 'Needs Improvement'
-  end
-{% endmacro %}
